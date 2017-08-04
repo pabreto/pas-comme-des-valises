4c139713e0cbe45618d54663571bc78983ec718d
pabreto <pabretonniere@hotmail.fr>
2017-08-04 23:36:24 +0200

    Add interactive reading of xml

--- Web/functions1.js
+++ Web/functions1.js
@@ -16,19 +16,11 @@ function initMap() {
 		var center = {lat: 41.591158, lng: 1.520862};
 		var zoom = 7;
 	}
-	
- //  var bounds = new google.maps.LatLngBounds();	
-   
+	   
    var map = new google.maps.Map(document.getElementById('map'), {
 	zoom: zoom,
     center: center,
   });
-  
-    // Instantiate a directions service.
- //   directionsService = new google.maps.DirectionsService ;
- //   directionsDisplay = new google.maps.DirectionsRenderer({
- //     map: map
- //   })
 	
   var departure = /** @type {!HTMLInputElement} */(
       document.getElementById('departure'));
@@ -97,19 +89,18 @@ function initMap() {
  infowindow.setContent('<div><strong>' + place.name );
 // infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
    infowindow.open(map, marker); 
-	
   });
    autocomplete_arrival.addListener('place_changed', function() {
-   // infowindow.close(); 
+    infowindow.close(); 
     marker_arrival.setVisible(false);
     var place = autocomplete_arrival.getPlace();
-	    var place2 = autocomplete.getPlace();
+	var place2 = autocomplete.getPlace();
 
     if (!place.geometry) {
       window.alert("Autocomplete's returned place contains no geometry");
       return;
     }
-
+/*
     // If the place has a geometry, then present it on a map.
     if (place.geometry.viewport) {
       map.fitBounds(place.geometry.viewport);
@@ -117,7 +108,9 @@ function initMap() {
       map.setCenter(place.geometry.location);
       map.setZoom(17);  // Why 17? Because it looks good.
     }
-    marker_arrival.setIcon(/** @type {google.maps.Icon} */({
+ */
+// marker_arrival.setIcon(/** @type {google.maps.Icon} */({
+/*
       url: place.icon,
       size: new google.maps.Size(71, 71),
       origin: new google.maps.Point(0, 0),
@@ -125,9 +118,9 @@ function initMap() {
       scaledSize: new google.maps.Size(35, 35)
     }));
     marker_arrival.setPosition(place.geometry.location);
-    marker_arrival.setVisible(true);
-
-    var address_arrival = '';
+    marker_arrival.setVisible(false);
+*/ 
+ var address_arrival = '';
     if (place.address_arrival_components) {
       address_arrival = [
         (place.address_arrival_components[0] && place.address_arrival_components[0].short_name || ''),
@@ -137,16 +130,16 @@ function initMap() {
     }
 
  
- infowindow.setContent('<div><strong>' + place.name );
+     marker.setVisible(false);
+
+        infowindow.setContent('<div><strong>' + place.name );
 // infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-   infowindow.open(map, marker_arrival); 
-	var directionsDisplay = new google.maps.DirectionsRenderer({
+ //       infowindow.open(map, marker_arrival); 
+    	var directionsDisplay = new google.maps.DirectionsRenderer({
           map: map
         });
-var chicago = {lat: 41.85, lng: -87.65};
-        var indianapolis = {lat: 39.79, lng: -86.14};
+
         // Set destination, origin and travel mode.
-		
         var request = {
           destination: place.geometry.location,
           origin: place2.geometry.location,
@@ -161,34 +154,26 @@ var chicago = {lat: 41.85, lng: -87.65};
           if (status == 'OK') {
             // Display the route on the map.
             directionsDisplay.setDirections(response);
+	//		directionsDisplay.setOptions( { suppressMarkers: true } );
           }
         });	
-  });
-  
-  
-	
-
-	
-
-  
-//  calculateAndDisplayRoute(directionsService, directionsDisplay, autocomplete, autocomplete_arrival);
-
- 
+  }); 
+ // loadXMLFile();
 }
- function calculateAndDisplayRoute(directionsService, directionsDisplay, autocomplete, autocomplete_arrival) {
-
-  //function calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB) {
-  directionsService.route({
-    origin: autocomplete,
-    destination: autocomplete_arrival,
-	//origin: pointA,
-    //destination: pointB,
-    travelMode: google.maps.TravelMode.DRIVING
-  }, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
+function loadXML(xmlFile) {
+    // load xml file
+    if (window.XMLHttpRequest) {
+       xhttp = new XMLHttpRequest();
+    } else {    // IE 5/6
+       xhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-  });
+
+    xhttp.open("GET", xmlFile, false);
+    xhttp.send();
+    xmlDoc = xhttp.responseXML; 
+
+    var uurloon = xmlDoc.getElementsByTagName("row")[0].childNodes[0].textContent;
+ //   var setloon = xmlDoc.getElementsByTagName("setloon")[0].childNodes[0].textContent;
+    console.log(uurloon); //give me "10 100"
 }
+
